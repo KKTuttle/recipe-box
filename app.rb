8496abda6fd7e5373a964634b4c9a1c33a754405
Bang Nguyen and Stephanie Gurung <bang@>
2016-07-20 15:02:13 -0700

    add a recipe and view ingredients

--- app.rb
+++ app.rb
@@ -22,14 +22,18 @@ end
 post('/recipes') do
   name = params.fetch('name')
   ingredient_ids = params.fetch('ingredient_ids')
-  @ingredients = []
-  ingredient_ids_integers = []
-  ingredient_ids.each() do |ingredient_id|
-   ingredient = Ingredient.find(ingredient_id.to_i())
-   @ingredients.push(ingredient)
-   ingredient_ids_integers.push(ingredient_id.to_i())
-  end
   instructions = params.fetch('instructions')
-  @recipe = Recipe.create({:name => name, :instruction => instructions, :ingredient_ids => ingredient_ids_integers})
-  redirect('/recipes/'.concat(@recipe.id().to_s()))
+  @recipe = Recipe.create({:name => name, :ingredient_ids => ingredient_ids, :instruction => instructions})
+  @recipes = Recipe.all()
+  erb(:recipes)
+end
+
+get('/recipes/:id') do
+  @recipe = Recipe.find(params.fetch('id').to_i())
+  erb(:recipe)
+end
+
+get('/recipes') do
+  @recipes = Recipe.all()
+  erb(:recipes)
 end
